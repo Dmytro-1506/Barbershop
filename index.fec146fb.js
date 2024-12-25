d75a22da860209a55f1712a6d5dafaf82697709f
Dmytro-1506 <kyunlik@gmail.com>
2024-12-25 17:51:29 +0000

    Deploying to gh-pages from @ Dmytro-1506/Barbershop@c9834599613418dca7781b31d641952ab4ce90f2 🚀

---
+++ index.fec146fb.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector(".js-open-menu"),t=document.querySelector(".js-close-menu"),n=document.querySelector(".js-menu-container"),c=()=>{const t="true"===e.getAttribute("aria-expanded")||!1;e.setAttribute("aria-expanded",!t),n.classList.toggle("is-open")};e.addEventListener("click",c),t.addEventListener("click",c)}();
+//# sourceMappingURL=index.fec146fb.js.map
