3251125542654e3264e494a2afacc2b83a3084f7
Dmytro-1506 <kyunlik@gmail.com>
2025-01-22 17:46:05 +0000

    Deploying to gh-pages from @ Dmytro-1506/Barbershop@c695e532604482663ed1504df543837ce5304efe 🚀

--- index.45061d71.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t,n,r;e=document.querySelector(".js-open-menu"),t=document.querySelector(".js-close-menu"),n=document.querySelector(".js-menu-container"),r=function(){var t="true"===e.getAttribute("aria-expanded")||!1;e.setAttribute("aria-expanded",!t),n.classList.toggle("is-open")},e.addEventListener("click",r),t.addEventListener("click",r)}();
-//# sourceMappingURL=index.45061d71.js.map
--- index.fec146fb.js
+++
@@ -1,2 +0,0 @@
-!function(){const e=document.querySelector(".js-open-menu"),t=document.querySelector(".js-close-menu"),n=document.querySelector(".js-menu-container"),c=()=>{const t="true"===e.getAttribute("aria-expanded")||!1;e.setAttribute("aria-expanded",!t),n.classList.toggle("is-open")};e.addEventListener("click",c),t.addEventListener("click",c)}();
-//# sourceMappingURL=index.fec146fb.js.map
